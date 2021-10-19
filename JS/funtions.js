ef999411855998e848424cbcfb819a4dab9d913b
Andresillo18 <andresv18@gmail.com>
2021-10-18 23:51:38 -0600

    JS agrandar img

--- JS/funtions.js
+++ JS/funtions.js
@@ -1,7 +1,5 @@
-
-
 // Events listeners que reaccionan al darle like al botón
-const clickHeart = document.getElementById('contenido');
+let clickHeart = document.getElementById('contenido');
 clickHeart.addEventListener("click", () => {
     document.getElementById('contenido').classList.add('corazon-activo');
     document.getElementById('corazon').classList.add('corazon-activo');
@@ -13,3 +11,17 @@ clickHeart.addEventListener("dblclick", () => {
     document.getElementById('text').classList.remove('corazon-activo');
 })
 
+// Función para agrandar la imagen
+function agrandarImg() {
+    document.getElementById('imgBig').classList.add('imgBig__activa');
+    // Le coloca un filtro a todo el contenedor menos a la imagen que no esta ahí
+    document.getElementById('contenedor').classList.add('contenedor-filtro')
+}
+
+// Función para cerrar la imagen en grande
+function closeImg() {
+    let imgBigger = document.getElementById('imgBig');
+    imgBigger.classList.remove('imgBig__activa');
+    // Le elimina la clase, el filtro del contenedor original
+    document.getElementById('contenedor').classList.remove('contenedor-filtro')
+}
